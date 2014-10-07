c54cca4dfb5cdec12099cbbc78873b371ea39215
John Tsiombikas <nuclear@member.fsf.org>
2014-10-07 21:57:34 +0300

    aaand ofcourse i forgot to add the new screen.h/screen.cc files from two commits back ...

---
+++ include/screen.h
@@ -0,0 +1,46 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#include "vec.h"
+#include "widget.h"
+
+namespace goatkit {
+
+class ScreenImpl;
+
+class Screen {
+private:
+	ScreenImpl *scr;
+
+public:
+	Screen();
+	virtual ~Screen();
+
+	virtual void set_position(float x, float y);
+	virtual void set_position(const Vec2 &pos);
+	virtual const Vec2 &get_position() const;
+	virtual void set_size(float x, float y);
+	virtual void set_size(const Vec2 &sz);
+	virtual const Vec2 get_size() const;
+	virtual const BBox &get_box() const;
+
+	virtual void add_widget(Widget *w);
+	virtual int get_widget_count() const;
+	virtual Widget *get_widget(int idx) const;
+	virtual Widget *get_widget(const char *name) const;
+
+	virtual void show();
+	virtual void hide();
+
+	virtual void draw() const;
+
+	// window system events used to generate widget events (see event.h)
+	void sysev_keyboard(int key, bool press);
+	// mouse position as reported by the window system. might fall outside
+	void sysev_mouse_button(int bn, bool press, float x, float y);
+	void sysev_mouse_motion(float x, float y);
+};
+
+}	// namespace goatkit
+
+#endif /* SCREEN_H_ */
